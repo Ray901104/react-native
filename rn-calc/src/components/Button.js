6c1bf52c82e0be541f1d3349c1ba7c7c597f768e
yrcho <ip323@naver.com>
2024-02-26 17:43:15 +0900

    터치 이벤트

--- rn-calc/src/components/Button.js
+++ rn-calc/src/components/Button.js
@@ -1,9 +1,23 @@
-import {Text} from "react-native";
+import {Pressable, Text} from "react-native";
 import PropTypes from "prop-types";
 
 const Button = ({title}) => {
     return (
-        <Text>{title}</Text>
+        <Pressable
+            onPressIn={() => console.log("In")}
+            onPressOut={() => console.log("Out")}
+            onPress={() => console.log("onPress")}
+            onLongPress={() => console.log("Long")}
+            delayLongPress={2000}
+            style={({pressed}) => {
+                return [
+                    {backgroundColor: "red"},
+                    pressed && {backgroundColor: "orange", opacity: 0.3},
+                ];
+            }}
+        >
+            <Text>{title}</Text>
+        </Pressable>
     );
 };
 
